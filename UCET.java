ffae4f3f4f2381dd725637477468fb96e8ecdfe3
heamanbelay <heamanbelay@gmail.com>
2023-02-03 11:58:18 +0300

    another commit

---
+++ UCET.java
@@ -0,0 +1,8 @@
+import java.rmi.*;
+public interface ucet extends Remote
+{
+public int function(int a,int b)throws RemoteException;
+public int power(int a,int b)throws RemoteException;
+public double log(int a)throws RemoteException;
+}
+
